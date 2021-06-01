b1be960f61798d5857788cc7ad7f4ad6c06f89f7
Bindu505 <61453963+Bindu505@users.noreply.github.com>
2021-06-01 19:24:36 +0530

    Update insertionsort.c

--- ADALAB5/insertionsort.c
+++ ADALAB5/insertionsort.c
@@ -1,32 +1,47 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+ int insertion_sort(int a[],int n)
+{
+	int y,i,j;
+for(i=1;i<n;i++)
+{
+y=a[i];
+j=i-1;
+while(j>=0&&a[j]>y)
+{
+a[j+1]=a[j];
+j=j-1;
+}
+a[j+1]=y;
+}
+}
+
 int main()
 {
-    int n, i, j, temp;
-    int arr[64];
- 
-    printf("Enter number of elements\n");
-    scanf("%d", &n);
- 
-    printf("Enter %d integers\n", n);
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    for (i = 1 ; i <= n - 1; i++)
-    {
-	    j = i;
-            while ( j > 0 && arr[j-1] > arr[j])
-            {	        
-                temp     = arr[j];
-                arr[j]   = arr[j-1];
-                arr[j-1] = temp;
-                j--;
-            }
-    }
-    printf("Sorted list in ascending order:\n");
-    for (i = 0; i <= n - 1; i++)
-    {
-        printf("%d\n", arr[i]);
-    }
-    return 0;
+int i,n,a[100];
+clock_t st,et;
+while(1)
+{
+printf("enter the number of the elements\n");
+scanf("%d",&n);
+printf("Enter the elements to be sorted\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+st=clock();
+insertion_sort(a,n);
+et=clock();
+
+printf("printing the sorted arary\n");
+{
+    for(i=0;i<n;i++)
+        printf("%d\n",a[i]);
+}
+
+double time_taken=((double)et-st)/CLOCKS_PER_SEC;
+printf("time taken for sorting %d elements is %f sec\n",n,time_taken);
+printf("\n");
+}
 }
